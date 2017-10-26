a8a0791d6c516169dd1c46c76406321223331b70
iuliiakotliar <iuliiakotliar@globallogic.com>
2017-10-26 16:49:14 +0300

    Task 4

--- src/test/java/Task04.java
+++ src/test/java/Task04.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
 import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
